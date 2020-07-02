656cfe193625dfa42b1c85f1489f796e0d1bbfc0
Laura Penstone <laura.penstone@queensu.ca>
2020-07-02 22:21:34 +0000

    change config for deploy

--- config/database.rb
+++ config/database.rb
@@ -10,7 +10,7 @@ configure do
       database: "db/db.sqlite3"
     }
   else
-    db_url = 'postgres://[your_postgres_url here]'
+    db_url = 'postgres://tkaidvptpqqwjr:cf1ea672b6584e32ac2d260c388dc091e3990b8befb9b97c15379f17a1d716f7@ec2-34-197-188-147.compute-1.amazonaws.com:5432/deopb96la3cudq'
     db = URI.parse(ENV['DATABASE_URL'] || db_url)
     set :database, {
       adapter: "postgresql",
